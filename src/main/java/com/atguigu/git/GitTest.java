22c9a2004cb7cd68494f75c57ced7361f251b80d
Mature <Mature@126.com>
2022-08-06 10:55:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author mature
+ * @create 2022-08-06 10:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git~");
+    }
+}
